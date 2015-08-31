0a7ad7908f3fcd504612bc57a82e1361424c7c24
Ueki Masaru <masaru.ueki@itage.co.jp>
2015-08-31 22:06:25 +0900

    p90

--- asmfunc.rs
+++ asmfunc.rs
@@ -5,6 +5,18 @@ pub fn io_hlt() {
     }
 }
 
+pub fn io_cli() {
+    unsafe {
+        asm!("cli");
+    }
+}
+
+pub fn io_sti() {
+    unsafe {
+        asm!("sti");
+    }
+}
+
 pub fn write_mem8(addr: u32, data: u8) {
     unsafe {
         asm!("mov $0, %al
@@ -12,7 +24,49 @@ pub fn write_mem8(addr: u32, data: u8) {
               mov %al, (%edx)"
              :
              : "r"(data), "r" (addr)
-             : "eax", "edx"
-             :"volatile");
+             : "eax", "edx");
+    }
+}
+
+pub fn io_out8(port: u16, data: u8) {
+    unsafe {
+        asm!("mov $0, %al
+              mov $1, %dx
+              out %al, %dx"
+             :
+             : "r"(data), "r"(port)
+             : "eax", "edx");
+    }
+}
+
+pub fn io_in8(port: u16) -> i16 {
+    let mut ret: i16;
+    unsafe {
+        asm!("mov $1, %dx
+              mov $$0, %eax
+              in %al, %dx"
+             : "={eax}"(ret)
+             : "r"(port)
+             : "eax", "edx");
+    }
+    ret
+}
+
+pub fn io_load_eflags() -> u32 {
+    let mut ret: u32;
+    unsafe {
+        asm!("pushf
+              pop $0"
+             : "=r" (ret));
+    }
+    ret
+}
+
+pub fn io_store_eflags(eflags: u32) {
+    unsafe {
+        asm!("push $0
+              popf"
+             :
+             : "r"(eflags));
     }
 }
--- bootpack.rs
+++ bootpack.rs
@@ -3,7 +3,7 @@ use asmfunc::*;
 
 pub fn hari_main() {
     for i in 0xa0000..0xaffff {
-        write_mem8(i, 15);
+        write_mem8(i, (i & 0x0f) as u8);
     }
 
     loop {

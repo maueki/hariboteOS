be2ea8422f067f2d4697b0b2cefaa819e9a736f0
Ueki Masaru <masaru.ueki@itage.co.jp>
2015-09-01 12:48:59 +0900

    write_mem8でasm!不使用に

--- asmfunc.rs
+++ asmfunc.rs
@@ -1,4 +1,6 @@
 
+use core::mem;
+
 pub fn io_hlt() {
     unsafe {
         asm!("hlt");
@@ -19,12 +21,8 @@ pub fn io_sti() {
 
 pub fn write_mem8(addr: u32, data: u8) {
     unsafe {
-        asm!("mov $0, %al
-              mov $1, %edx
-              mov %al, (%edx)"
-             :
-             : "r"(data), "r" (addr)
-             : "eax", "edx");
+        let p: &mut u8 = mem::transmute(addr);
+        *p = data;
     }
 }
 
